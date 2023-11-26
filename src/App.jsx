82212faf765abdbe0f0ab44c329f3169dcb07079
De-Hype <iamsocialhype@gmail.com>
2023-11-25 18:39:47 -0800

    God abeg

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-<<<<<<< HEAD
 // import "./App.css";
 import Screen from "./Components/Screen";
-=======
-import './App.css'
-import Screen from './Components/Screen'
->>>>>>> 20a16bfdaf8a2a127939afb31fcbb05277b9f561
-
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-<<<<<<< HEAD
           <Route exact path="/" element={<Screen />} />
         </Routes>
       </BrowserRouter>
-=======
-            <Route exact path="/" element={<Screen/>} />
-          </Routes>
-      </BrowserRouter>
-     
->>>>>>> 20a16bfdaf8a2a127939afb31fcbb05277b9f561
+           
     </>
   );
 }
