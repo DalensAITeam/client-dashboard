30fb0135006c400a5993618ea98a824bc7499ff9
InimfonUdoh <okoninimfon2001@gmail.com>
2023-11-26 05:44:34 -0800

    Done with Notification Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Notification from './pages/notification'
+import ToggleButtonOnOff from './pages/notification'
+
+
 
 function App() {
 
 
   return (
    <div>
-    <Notification />
+    <ToggleButtonOnOff/>
    </div>
   )
 }
--- src/pages/notification.jsx
+++ src/pages/notification.jsx
@@ -1,58 +1,50 @@
-import React from 'react'
+import React, { useState } from "react";
+import Switch from "../assets/components/switch";
 
-function Notification() {
+
+
+
+const ToggleButtonOnOff = () => {
+    const [isOff, setIsOff] = useState(true);
+
+
     return (
         <div>
             <div className="text-zinc-900 text-[40px] font-medium font-['Inter']">Notifications</div>
-            <div className="w-[750px] h-[81px] p-6 bg-white shadow justify-center items-center gap-[396px] inline-flex">
+
+
+            <div className="w-[750px] h-[81px] p-6 bg-white shadow justify-center items-center gap-[396px] inline-flex border-solid border-2 mb-5">
                 <div className="text-neutral-800 text-[22px] font-normal font-['Poppins']">Animals Health Status</div>
                 <div className="justify-start items-center inline-flex">
-                    <div className="w-8 h-5 p-0.5 bg-blue-800 rounded-[120px] justify-end items-center flex">
-                        <div className="w-4 h-4 bg-white rounded-[100px]"></div>
-                    </div>
-                    <div className="pl-2 justify-start items-start flex">
-                        <div className="text-indigo-300 text-base font-normal font-['Inter'] leading-tight">ON</div>
-                    </div>
+                    <Switch />
                 </div>
             </div>
 
-            <div className="w-[750px] h-[81px] p-6 bg-white shadow justify-center items-center gap-[475px] inline-flex">
+            <div className="w-[750px] h-[81px] p-6 bg-white shadow justify-center items-center gap-[475px] inline-flex border-solid border-2 mb-5">
                 <div className="text-neutral-800 text-[22px] font-normal font-['Poppins']">Feeding Status</div>
                 <div className="justify-start items-center inline-flex">
-                    <div className="w-8 h-5 p-0.5 bg-blue-800 rounded-[120px] justify-end items-center flex">
-                        <div className="w-4 h-4 bg-white rounded-[100px]"></div>
-                    </div>
-                    <div className="pl-2 justify-start items-start flex">
-                        <div className="text-indigo-300 text-base font-normal font-['Inter'] leading-tight">ON</div>
-                    </div>
+                    <Switch />
                 </div>
             </div>
 
-            <div className="w-[750px] h-[81px] p-6 bg-white shadow justify-center items-center gap-[478px] inline-flex">
+            <div className="w-[750px] h-[81px] p-6 bg-white shadow justify-center items-center gap-[476px] inline-flex border-solid border-2 mb-5">
                 <div className="text-neutral-800 text-[22px] font-normal font-['Poppins']">Feeding Alarm</div>
                 <div className="justify-start items-center inline-flex">
-                    <div className="w-8 h-5 p-0.5 bg-blue-800 rounded-[120px] justify-end items-center flex">
-                        <div className="w-4 h-4 bg-white rounded-[100px]"></div>
-                    </div>
-                    <div className="pl-2 justify-start items-start flex">
-                        <div className="text-indigo-300 text-base font-normal font-['Inter'] leading-tight">ON</div>
-                    </div>
+                    <Switch />
                 </div>
             </div>
 
-            <div className="w-[750px] h-[81px] p-6 bg-white shadow justify-center items-center gap-[478px] inline-flex">
+            <div className="w-[750px] h-[81px] p-6 bg-white shadow justify-center items-center gap-[478px] inline-flex border-solid border-2">
                 <div className="text-neutral-800 text-[22px] font-normal font-['Poppins']">Feeding Alarm</div>
                 <div className="justify-start items-center inline-flex">
-                    <div className="w-8 h-5 p-0.5 bg-blue-800 rounded-[120px] justify-end items-center flex">
-                        <div className="w-4 h-4 bg-white rounded-[100px]"></div>
-                    </div>
-                    <div className="pl-2 justify-start items-start flex">
-                        <div className="text-indigo-300 text-base font-normal font-['Inter'] leading-tight">ON</div>
-                    </div>
+                    <Switch />
                 </div>
             </div>
         </div>
-    )
-}
 
-export default Notification
+
+    );
+};
+
+
+export default ToggleButtonOnOff
