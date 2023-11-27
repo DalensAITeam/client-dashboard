e754eb9d5175e2ce321598b2ff534e478a25f2cd
InimfonUdoh <okoninimfon2001@gmail.com>
2023-11-27 08:07:54 -0800

    Notification Page Done

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import ToggleButtonOnOff from './pages/notification'
-
+import Pricing from './pages/pricing'
 
 
 function App() {
@@ -12,6 +12,7 @@ function App() {
   return (
    <div>
     <ToggleButtonOnOff/>
+    {/* <Pricing/> */}
    </div>
   )
 }
---
+++ src/pages/pricing.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+function Pricing() {
+    return (
+        <div className='sm:block'>
+            <div class="left-[741px] top-[156px] absolute text-zinc-900 text-[40px] font-medium font-['Inter']">Pricing</div>
+            <div class="w-[317px] h-[708px] left-[636px] top-[236px] absolute">
+                <div class="w-[317px] h-[708px] left-0 top-0 absolute bg-stone-300"></div>
+                <div class="left-[112px] top-[48px] absolute text-center text-white text-2xl font-medium font-['Poppins']">Regular</div>
+                <div class="h-[27px] left-[117.50px] top-[100px] absolute justify-start items-start gap-px inline-flex">
+                    <div class="text-center text-white text-2xl font-normal font-['Poppins']">N</div>
+                    <div class="w-[65px] h-[49px] text-center text-white text-3xl font-normal font-['Belanosima']">******</div>
+                </div>
+                <div class="left-[43.86px] top-[414.14px] absolute origin-top-left -rotate-45 text-center text-white text-4xl font-medium font-['Poppins']">Coming Soon</div>
+                <div class="h-[58px] px-[30px] py-5 left-[85px] top-[602px] absolute bg-lime-500 rounded-[5px] justify-center items-center gap-2.5 inline-flex">
+                    <div class="text-white text-[19px] font-medium font-['Poppins']">Add Plan</div>
+                </div>
+            </div>
+            <div class="w-[317px] h-[708px] left-[636px] top-[976px] absolute">
+                <div class="w-[317px] h-[708px] left-0 top-0 absolute bg-white border border-stone-300"></div>
+                <div class="left-[99px] top-[48px] absolute text-center text-blue-200 text-2xl font-normal font-['Poppins']">Enterprise</div>
+                <div class="h-[27px] left-[117.50px] top-[100px] absolute justify-start items-start gap-px inline-flex">
+                    <div class="text-center text-stone-300 text-2xl font-normal font-['Poppins']">N</div>
+                    <div class="w-[65px] h-[49px] text-center text-stone-300 text-3xl font-normal font-['Belanosima']">******</div>
+                </div>
+                <div class="left-[43.86px] top-[414.14px] absolute origin-top-left -rotate-45 text-center text-neutral-200 text-4xl font-medium font-['Poppins']">Coming Soon</div>
+                <div class="h-[58px] px-[30px] py-5 left-[85px] top-[602px] absolute bg-lime-500 rounded-[5px] justify-center items-center gap-2.5 inline-flex">
+                    <div class="text-white text-[19px] font-medium font-['Poppins']">Add Plan</div>
+                </div>
+            </div>
+            <div class="w-[317px] h-[708px] left-[969px] top-[236px] absolute">
+                <div class="w-[317px] h-[708px] left-0 top-0 absolute bg-white border border-stone-300"></div>
+                <div class="left-[103px] top-[48px] absolute text-center text-blue-200 text-2xl font-medium font-['Poppins']">Premium</div>
+                <div class="h-[27px] left-[117.50px] top-[100px] absolute justify-start items-start gap-px inline-flex">
+                    <div class="text-center text-stone-300 text-2xl font-normal font-['Poppins']">N</div>
+                    <div class="w-[65px] h-[49px] text-center text-stone-300 text-3xl font-normal font-['Belanosima']">******</div>
+                </div>
+                <div class="left-[43.86px] top-[414.14px] absolute origin-top-left -rotate-45 text-center text-neutral-200 text-4xl font-medium font-['Poppins']">Coming Soon</div>
+                <div class="h-[58px] px-[30px] py-5 left-[85px] top-[602px] absolute bg-lime-500 rounded-[5px] justify-center items-center gap-2.5 inline-flex">
+                    <div class="text-white text-[19px] font-medium font-['Poppins']">Add Plan</div>
+                </div>
+            </div>
+        </div>
+
+  )
+}
+
+export default Pricing
