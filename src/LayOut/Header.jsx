ae3fd68d553c20473898b4f1caf700451b3bfefe
Blessing Eno <bublessingsmart@gmail.com>
2024-02-07 23:58:15 +0100

    modified:   src/LayOut/Header.jsx     changed the bell and user icon

--- src/LayOut/Header.jsx
+++ src/LayOut/Header.jsx
@@ -1,30 +1,33 @@
 import headline from "../assets/headline.svg";
-import bell from "../assets/bell.svg";
-import user from "../assets/user.svg";
+import { FaBell } from "react-icons/fa";
+// import bell from "../assets/bell.svg";
+// import user from "../assets/user.svg";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { SideNavToggle } from "../Redux/ActionSlice";
+
+
 const Header = () => {
   const dispatch = useDispatch();
   const openSideNav = useSelector((state) => state.actions.openSideNav);
 
   return (
-    <div className="flex items-center justify-between [box-shadow:2px_3px_4px_black] px-[.9rem] py-[.4rem]">
+    <div className="flex items-center justify-between border-b-2 px-[.9rem] py-[.4rem]">
       <img
         onClick={() => dispatch(SideNavToggle())}
         className="h-[40px] w-[40px] cursor-pointer"
         src={headline}
         alt=""
       />
-      <div className="flex gap-[1.6rem]">
-        <Link onClick={() => openSideNav && dispatch(SideNavToggle())} to="/">
-          <img className="h-[40px] w-[40px]" src={bell} alt="" />
+      <div className="flex gap-8 items-center ">
+        <Link onClick={() => openSideNav && dispatch(SideNavToggle())} to="/" className="text-black">
+          <FaBell size={20} />
         </Link>
         <Link
           onClick={() => openSideNav && dispatch(SideNavToggle())}
           to="/profile"
         >
-          <img className="h-[40px] w-[40px]" src={user} alt="" />
+          <button className="rounded-full w-10 h-10 bg-lime-300">{'HF'}</button>
         </Link>
       </div>
     </div>
