c8a658fbde29479ee7eebb32a28224c5253f1741
Blessing Eno <bublessingsmart@gmail.com>
2024-02-08 16:39:35 +0100

    modified:   src/pages/profileSetting/userProfileSetting.jsx

--- src/pages/profileSetting/userProfileSetting.jsx
+++ src/pages/profileSetting/userProfileSetting.jsx
@@ -9,7 +9,7 @@ function UserProfileSetting() {
     navigator("/");
   }
   return (
-    <div className="flex flex-col items-start gap-5 w-full p-3 h-full">
+    <div className="flex flex-col items-start gap-5 w-full p-3 ">
       <h2 className="  flex flex-row items-center font-inter text-2xl gap-5 font-semibold text-gray-800">
         <FaArrowLeft
           className=" flex md:hidden w-17 h-5 cursor-pointer"
@@ -17,11 +17,13 @@ function UserProfileSetting() {
         />
         Profile Settings
       </h2>
-      <div className="flex  flex-row align-center sm:justify-between justify-center gap-5 w-full shadow-md p-5">
-        <div className="flex sm:flex-row flex-col align-center justify-center gap-5">
-          <div className="relative w-32 h-32 rounded-full border-4 border-green-500 flex items-center justify-center">
+      <div className="flex flex-row items-center sm:justify-between justify-center gap-5 w-full shadow-md p-5">
+        <div className="flex sm:flex-row flex-col justify-center gap-5">
+          <div >
             <FaEdit className="absolute right-[-20px] top-[-10px] w-6 h-6 text-green-500 cursor-pointer sm:hidden" />
-            <h6>HF</h6>
+            <img  src={""} 
+                  alt="HF" 
+                  className="w-32 h-32 rounded-full border-4 border-green-500 flex items-center justify-center"/>
           </div>
           <div className="flex flex-col align-center justify-center ">
             <h4 className="flex items-center justify-center font-poppins text-lg font-semibold text-gray-800">
@@ -32,11 +34,8 @@ function UserProfileSetting() {
             </h6>
           </div>
         </div>
-        <div className="hidden flex-col items-center justify-center sm:flex flex-col ">
-          <h3 className="flex flex-row items-center justify-center text-xl font-semibold border border-gray-800 rounded-custom h-10 w-120 p-5 cursor-pointer font-poppins text-base font-semibold text-left">
-            Update photo
-          </h3>
-        </div>
+        <button className="hidden sm:flex items-center sm:border sm:border-gray-800 md:text-xl md:font-semibold h-10 px-3">Update photo
+        </button>
       </div>
       <UserProfileDetailsSection />
     </div>
